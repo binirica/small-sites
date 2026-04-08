8045b43be6cc7b8297999f9ce40de869b6fcf80e
binirica <lrusso@risd.edu>
2026-04-08 16:25:51 -0400

    commit

--- loop/loop.js
+++ loop/loop.js
@@ -1,1 +1,9 @@
-console.log("This is the loop site!");
+const images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
+const imgElement = document.getElementById("loopImage");
+
+function changeImage() {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    imgElement.src = images[randomIndex];
+}
+
+setInterval(changeImage, 3000);
--- time/time.js
+++ time/time.js
@@ -1,1 +1,11 @@
-console.log("This is the time site!");
+const images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
+const imgElement = document.getElementById("loopImage");
+
+function changeImage() {
+    // Generate random index between 0 and array length
+    const randomIndex = Math.floor(Math.random() * images.length);
+    imgElement.src = images[randomIndex];
+}
+
+// Loop every 3000ms (3 seconds)
+setInterval(changeImage, 3000);
